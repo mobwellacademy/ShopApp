17a3c8eff84e40137c8e69d5efbd7beb04f69cc8
dario <dario.botas@gmail.com>
2015-02-09 21:58:32 +0000

    First Commit

--- app/src/main/java/org/mobwell/academy/shopapp/MainActivity.java
+++ app/src/main/java/org/mobwell/academy/shopapp/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private String name;
+    private String lastName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
